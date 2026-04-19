dc0900f27feffa5de0d7b63257a9bd907e66be4f
Parth8818 <pkasodariya8818@gmail.com>
2026-04-18 21:02:21 -0400

    Update login.php

--- login.php
+++ login.php
@@ -25,21 +25,35 @@ if ($_POST) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
-   <link rel="stylesheet" href="style.css">
+    <title>Login | Clothing Store</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Login</h1>
-    <p><a href="index.php">&larr; Home</a></p>
-    <?php if ($error): ?>
-        <p style="color:red;"><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
-    <form method="POST">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button>Login</button>
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a></p>
-    <p><small>Staff login: admin@test.com / admin123</small></p>
+    <div class="container" style="max-width: 400px;"> 
+        <h1>Login</h1>
+        
+        <p><a href="index.php" style="text-decoration: none;">&larr; Home</a></p>
+
+        <?php if ($error): ?>
+            <p class="error-msg"><?= htmlspecialchars($error) ?></p>
+        <?php endif; ?>
+
+        <form method="POST">
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email Address" required>
+            </div>
+            
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary" style="width: 100%;">Login</button>
+        </form>
+
+        <div class="text-center mt-20">
+            <p>Don't have an account? <a href="register.php">Register here</a></p>
+            <p><small style="color: gray;">Staff: admin@test.com / admin123</small></p>
+        </div>
+    </div>
 </body>
 </html>
